89f64c0f740b54c8c88df99d9c2b28cbab4d1be6
kenzosalatiel <161518315+kenzosalatiel@users.noreply.github.com>
2024-11-14 16:56:54 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-function criaCartao(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
-    let cartao= document.createElement('article')
-    cartao.className=('cartao')
-    cartao.innerHTML=
-    <div class="cartao__conteudo"> 
-        <h3>Programação</h3>
-        <div class="cartao__conteudo__pergunta">9
-        <p>O que é JavaScrit?</p>
-     </div>
-     <div class="cartao__conteudo__resposta">
-       <p>O JavaScript é uma linguagem de programação</p></div>     
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
+
 }
